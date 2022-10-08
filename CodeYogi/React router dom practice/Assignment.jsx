e02e70c87818d07d91cd55b865925fc27c816aa6
Arnav Srivastava <80662620+arnav-srivastava@users.noreply.github.com>
2022-10-08 22:26:52 +0530

    daily prac web

---
+++ CodeYogi/React router dom practice/Assignment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Assignment() {
+  return (
+    <div className='w-full text-3xl bg-red-500 h-screen'>Assignment Page</div>
+  )
+}
+
+export default Assignment;
---
+++ CodeYogi/React router dom practice/Lecture.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Lecture() {
+  return (
+    <div className='w-full text-3xl bg-green-500 h-screen'>Lecture Page</div>
+  )
+}
+
+export default Lecture;
